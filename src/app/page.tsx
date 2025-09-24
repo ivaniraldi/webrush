85f4e3937dc14d8c8c32bf6c49ba7a4ac9561328
Ivan Iraldi <iraldiban@gmail.com>
2025-09-24 18:34:54 +0000

    jaja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HowItWorks from '@/components/landing/how-it-works';
 import Faq from '@/components/landing/faq';
 import Footer from '@/components/landing/footer';
 import FloatingWhatsapp from '@/components/landing/floating-whatsapp';
- 
+  
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
